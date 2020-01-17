fde189b78486dc9497fdc4f401e159e92798403e
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-17 16:50:26 +0100

    click-prev

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ $(document).ready(
         clickNext();
       }
     );
-    // $('.prev').click(
-    //   function() {
-    //     clickPrev();
-    //   }
-    // );
+    $('.prev').click(
+      function() {
+        clickPrev();
+      }
+    );
   }
 );
 
@@ -22,4 +22,29 @@ function clickNext() {
   //cerchi
   var circleActive = $('i.active');
   var circleNext = circleActive.next();
+  if (imageActive.hasClass('last') && circleActive.hasClass('last')) {
+    imageNext = $('img.first');
+    circleNext = $('i.first');
+  }
+  imageActive.removeClass('active');
+  imageNext.addClass('active');
+  circleActive.removeClass('active');
+  circleNext.addClass('active');
+}
+
+function clickPrev() {
+  //immagini
+  var imageActive = $('img.active');
+  var imagePrev = imageActive.prev();
+  //cerchi
+  var circleActive = $('i.active');
+  var circlePrev = circleActive.prev();
+  if (imageActive.hasClass('first') && circleActive.hasClass('first')) {
+    imagePrev = $('img.last');
+    circlePrev = $('i.last');
+  }
+  imageActive.removeClass('active');
+  imagePrev.addClass('active');
+  circleActive.removeClass('active');
+  circlePrev.addClass('active');
 }
